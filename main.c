6ab444705cd73685dcffa2d5eeca114100027a65
pwntheweb <ezra005@proton.me>
2023-11-11 09:51:57 +0000

    fixes

--- main.c
+++ main.c
@@ -6,20 +6,20 @@ int main(void)
 	size_t bufsize = 0;
 	
 	char *envp[] = {"PATH=/bin", "HOME=/home/vagrant", NULL};
-
-    while (1) 
-    {
-	    printf("my_shell$ ");
-	    getline(&buffer, &bufsize, stdin);
-	    
-	    if (strcmp(buffer, "exit\n") == 0) 
-	    {
-		    free(buffer);
-		    break;
-        }
-	    index_function(buffer, envp);
-    }
-    free(buffer);
-    return (0);
+	
+	while (1)
+	{
+		printf("my_shell$ ");
+		getline(&buffer, &bufsize, stdin);
+		
+		if (strcmp(buffer, "exit\n") == 0)
+		{
+			free(buffer);
+			break;
+		}
+		index_function(buffer, envp);
+	}
+	
+	free(buffer);
+	return (0);
 }
-

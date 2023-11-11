7d52007003cbc8c7700f8abe2146c15bca9ea1da
pwntheweb <ezra005@proton.me>
2023-11-11 09:11:45 +0000

    fixes

--- main.c
+++ main.c
@@ -2,24 +2,23 @@
 
 int main(void)
 {
-    char *buffer = NULL;
-    size_t bufsize = 0;
+	char *buffer = NULL;
+	size_t bufsize = 0;
+	
+	char *envp[] = {"PATH=/bin", "HOME=/home/vagrant", NULL};
 
-    char *envp[] = {"PATH=/bin", "HOME=/home/vagrant", NULL};
-
-    while (1) {
-        printf("my_shell$ ");
-
-        getline(&buffer, &bufsize, stdin);
-
-        if (strcmp(buffer, "exit\n") == 0) {
-            free(buffer);
-            break;
+    while (1) 
+    {
+	    printf("my_shell$ ");
+	    getline(&buffer, &bufsize, stdin);
+	    
+	    if (strcmp(buffer, "exit\n") == 0) 
+	    {
+		    free(buffer);
+		    break;
         }
-
-        index_function(buffer, envp);
+	    index_function(buffer, envp);
     }
-
     free(buffer);
     return (0);
 }

e7e26f8733e21b338a5108dddf2cbc2e753457fe
pwntheweb <ezra005@proton.me>
2023-11-11 09:57:46 +0000

    fixed betty

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 #include "my_shell.h"
-
+/**
+ * main - Entry point
+ * index_function: determine appropriate action
+ * Return: 0 Success
+ */
 int main(void)
 {
 	char *buffer = NULL;
 	size_t bufsize = 0;
-	
 	char *envp[] = {"PATH=/bin", "HOME=/home/vagrant", NULL};
-	
+
 	while (1)
 	{
 		printf("my_shell$ ");
 		getline(&buffer, &bufsize, stdin);
-		
+
 		if (strcmp(buffer, "exit\n") == 0)
 		{
 			free(buffer);
@@ -19,7 +22,6 @@ int main(void)
 		}
 		index_function(buffer, envp);
 	}
-	
 	free(buffer);
 	return (0);
 }
